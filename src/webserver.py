98b957b64bce2baf549fd32e2a1ff29801a3c302
g4s1kk <g4s1kk@gmail.com>
2025-04-02 12:11:39 +0300

    dev

--- src/webserver.py
+++ src/webserver.py
@@ -1,9 +1,24 @@
 import microdot
 from microdot import websocket
 
-from src.config import config as cfg
+from src.config import config, Config
 
 
-web_server = microdot.Server()
+class WebServer(microdot.Microdot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def add_checkpoint_app_link(self, checkpoint_app):
+        self.checkpoint_app = checkpoint_app
+
+    def add_cfg_link(self, cfg:Config=config):
+        self.cfg = cfg
+
+web_server = WebServer()
+web_server.add_cfg_link(config)
 
 # add handlers here
+
+@web_server.get("/")
+def send_frontend(request):
+
